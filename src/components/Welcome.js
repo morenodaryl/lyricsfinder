20287db95bc27715081dd08c4aa0fd886c78e907
Daryl Moreno <morenodaryl@hotmail.com>
2018-04-25 22:58:56 -0700

    Adding github repository to the welcome component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ const Welcome = (props) =>
     <div className="right-body text-center">
         <h1 className="mt-4">Lyrics finder</h1>
         <p className="">by <a href="https://twitter.com/morenodaryl">@morenodaryl</a></p>
-        <a className="mr-1" target="_blank" href="#"><img src="github.png" alt="Github Repository" /></a>
+        <a className="mr-1" target="_blank" href="https://github.com/morenodaryl/lyricsfinder"><img src="github.png" alt="Github Repository" /></a>
         <a className="mr-1" target="_blank" href="https://www.twitter.com/morenodaryl"><img src="twitter.png" alt="Instagram personal account" /></a>
         <a className="mr-1" target="_blank" href="https://www.instagram.com/morenodaryl"><img src="instagram.png" alt="Twitter personal account" /></a>
     </div>
