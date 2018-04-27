8383399e40f20e3fd1c74b301352a0eb2b7c0c80
Daryl Moreno <morenodaryl@hotmail.com>
2018-04-26 17:43:42 -0700

    validating the search form inputs

--- src/containers/SearchForm.js
+++ src/containers/SearchForm.js
@@ -6,7 +6,8 @@ class SearchForm extends Component {
         super(props);
         this.state = {
             artist: this.props.artist,
-            song: this.props.song
+            song: this.props.song,
+            submitEnabled: false
         }
     }
 
@@ -14,7 +15,7 @@ class SearchForm extends Component {
         if (previousProps !== this.props){
             const { match } = this.props;
             if (match != null && match.params.artist != null && match.params.song != null) {
-                this.setState({ artist: match.params.artist, song: match.params.song, updated: true});
+                this.setState({ artist: match.params.artist, song: match.params.song, submitEnabled: true});
             }else{
                 this.setState({artist: '', song: ''})
             }
@@ -22,28 +23,38 @@ class SearchForm extends Component {
     }
 
     updateInputValue(event) {
+        let {artist, song} = this.state;
         switch (event.target.name) {
             case 'artist':
-                this.setState({artist: event.target.value});
-                break; 
+                artist = event.target.value;
+                this.setState({artist});
+                break;
             case 'song':
-                this.setState({song: event.target.value });
-                break; 
+                song = event.target.value;
+                this.setState({song});
+                break;
             default:
                 break;
+        }
+        if(artist !== "" && song !== ""){
+            this.setState({ submitEnabled: true });
+        }else{
+            this.setState({ submitEnabled: false });
         }
     }
 
     submitForm(event){
         event.preventDefault();
-        this.props.history.push('/' + this.state.artist + '/' + this.state.song);
+        if (this.state.artist   !== "" && this.state.song !== "") {
+            this.props.history.push('/' + this.state.artist + '/' + this.state.song);
+        }
     }
 
     render() { 
         return (<form onSubmit={this.submitForm.bind(this)}>
                     <input value={this.state.artist} onChange={this.updateInputValue.bind(this)} name='artist' className="form-control" type="text" placeholder="Artist" />
                     <input value={this.state.song} onChange={this.updateInputValue.bind(this)} name='song' className="form-control" type="text" placeholder="Name of the song" />
-                    <button type="submit" className="btn btn-light btn-block">Search</button>
+                    <button type="submit" className="btn btn-light btn-block" disabled={!this.state.submitEnabled}>Search</button>
                 </form>
          )
     }
